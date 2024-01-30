2aa5f488d612f92c1da4a8e033df1fe42931c6f2
katevolkova <kate.volkova@nextgenclearing.com>
2024-01-30 10:50:17 +0100

    add e2e

--- e2etest/open-browser-test.mjs
+++ e2etest/open-browser-test.mjs
@@ -1,5 +1,4 @@
 import { Builder } from 'selenium-webdriver';
-import * as chrome from 'selenium-webdriver/chrome.js';
 import { describe, it, before, after } from 'mocha';
 
 describe('Open Browser Test', function() {
@@ -7,23 +6,17 @@ describe('Open Browser Test', function() {
   let driver;
 
   before(async function() {
-    let options = new chrome.Options();
-    options.addArguments('--headless');
-    options.addArguments('--disable-gpu');
-    options.addArguments('--no-sandbox');
-    options.addArguments('--disable-dev-shm-usage');
-
-    driver = await new Builder()
-    .forBrowser('chrome')
-    .setChromeOptions(options)
-    .build();
+    driver = await new Builder().forBrowser('chrome').build();
   });
 
   after(async function() {
-    await driver.quit();
+    if (driver) {
+      await driver.quit();
+    }
   });
 
   it('should open Chrome browser and load localhost:3000', async function() {
+
     await driver.get('http://localhost:3000');
   });
 });
