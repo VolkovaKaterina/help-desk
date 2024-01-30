4823bc27881c883b0eaf6243cf8d410cd8294fca
katevolkova <kate.volkova@nextgenclearing.com>
2024-01-30 11:27:45 +0100

    add e2e

--- e2etest/open-browser-test.mjs
+++ e2etest/open-browser-test.mjs
@@ -3,10 +3,21 @@ import { describe, it, before, after } from 'mocha';
 
 describe('Open Browser Test', function() {
   this.timeout(20000);
-  let driver;
+  let options = new chrome.Options();
+  options.addArguments('--headless');
+  options.addArguments('--disable-gpu');
+  options.addArguments('--no-sandbox');
+  options.addArguments('--disable-dev-shm-usage');
+  options.addArguments('--disable-extensions');
+  options.addArguments('--window-size=1920,1080');
+
+  let driver
 
   before(async function() {
-    driver = await new Builder().forBrowser('chrome').build();
+    driver = await new Builder()
+    .forBrowser('chrome')
+    .setChromeOptions(options)
+    .build();
   });
 
   after(async function() {
