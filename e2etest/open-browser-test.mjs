78dc75f1b0c23d4a7fb0109bc89e80f3e744f481
katevolkova <kate.volkova@nextgenclearing.com>
2024-03-19 23:01:15 +0100

    add e2e

--- e2etest/open-browser-test.mjs
+++ e2etest/open-browser-test.mjs
@@ -1,20 +1,28 @@
-import { Builder } from 'selenium-webdriver';
-import { describe, it, before, after } from 'mocha';
+import {Builder} from "selenium-webdriver";
+import {describe, it, before, after} from "mocha";
+import {Options} from "selenium-webdriver/ie.js";
 
-describe('Open Browser Test', function() {
+describe("Open Browser Test", function () {
   this.timeout(20000);
   let driver;
 
-  before(async function() {
-    driver = await new Builder().forBrowser('chrome').build();
+  before(async function () {
+    const options = new Options();
+    options.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage", "--remote-debugging-pipe"); // Add headless argument
+    options.addArguments("window-size=1920,1080"); // Specify window size if needed
+
+    driver = await new Builder()
+    .forBrowser("chrome")
+    .setChromeOptions(options)
+    .build();
   });
 
-  after(async function() {
+  after(async function () {
     await driver.quit();
   });
 
-  it('should open Chrome browser and load localhost:3000', async function() {
+  it("should open Chrome browser and load localhost:3000", async function () {
 
-    await driver.get('http://localhost:3000');
+    await driver.get("http://localhost:3000");
   });
 });
